ddc8cdf1baa15be04ea8ecb90affb8e751dc72d4
Josh <joshuamyers1097@gmail.com>
2022-10-12 23:21:19 +0100

    added multithreading

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 Simple python port scanner.
 Programmed by Joshua Myers 2022
-This port scanner (is going to) use multithreading to scan as many ports as fast as possible. 
+This port scanner uses multithreading to scan as many ports as fast as possible. 
 The port scanner will display all open ports with a given ip, exposing any unneccesarily opened ports and hence, securty threats to the network. 
 Do not use the port scanner for any network that you dont own or have rights to use. 
 Port scanning unauthorised networks is illegal.
--- index.py
+++ index.py
@@ -1,7 +1,12 @@
 import socket
+import threading 
+from queue import Queue # stops ports scanning multiple times
 
 # uses sockets to connect to a port and then display that the port in question is open
 target = "127.0.0.1" #insert ip here (localhost used)
+
+queue = Queue()
+openPorts = []
 
 def portscan(port):
     try:
@@ -11,11 +16,35 @@ def portscan(port):
     except:
         return False 
 
-# scans every port in the range
-for port in range(1,1024):
-    result = portscan(port)
-    if result:
-        print("Port {} is open".format(port))
-    else:
-        print("Port {} is closed".format(port))
+def fillQueue(portList):
+    for port in portList:
+        queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("Port {} is open".format(port))
+            openPorts.append(port)
+        
+portList = range(1, 1024)
+fillQueue(portList)
+
+threadList = []
+
+# creates 100 threads
+for t in range(100):
+    # refers to worker function without calling it
+    thread = threading.Thread(target=worker)
+    threadList.append(thread)
+
+# opens all threads
+for thread in threadList:
+    thread.start()
+
+# waits for all threads to finish until it continues with code
+for thread in threadList:
+    thread.join()
+
+print("Open ports are: ", openPorts)
 
